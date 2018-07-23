87eb9f6dfc1c81e3f16122fbd13e081b06851ca7
Nico van der Linden <nicolinden@gmail.com>
2018-07-23 09:31:49 +0200

    tet

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://boiling-forest-44762.herokuapp.com/';
 }
  function getQueryStringParameterByName(name, url) {
    if (!url) url = window.location.href;
